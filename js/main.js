96830274229378fefb36d30ed7c6b0876f0404a5
Konstantin Chumachenko <34819747+kostyachuma@users.noreply.github.com>
2020-08-18 20:36:36 +0300

    18.08.2020

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.feedback-slider').slick({
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev">Привет</button>',
+        nextArrow: '<button type="button" class="slick-next">Сюда</button>'
+    });
+
+})
+
+
+
--- main.js
+++
@@ -1,1 +0,0 @@
-$('.feedback__slider').slick();
