d72f80a6882a87338bc7f4315d57e2e588739165
Inna Taranenko <inna.taranenko@gmail.com>
2020-08-20 19:26:56 +0300

    Change landing page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.feedback-slider').slick({
         slidesToShow: 1,
-        prevArrow: '<button type="button" class="slick-prev">Привет</button>',
-        nextArrow: '<button type="button" class="slick-next">Сюда</button>'
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
     });
 
 })
